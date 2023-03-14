d96b522bcb37b7a00b23f528db57919b4fe70e2f
Eangheng <92841997+Eangheng0302@users.noreply.github.com>
2023-03-14 11:29:30 +0700

    Update day3.py

--- day3.py
+++ day3.py
@@ -2,6 +2,6 @@ import streamlit as st
 # create button 
 st.header("Button")
 if st.button("Say Hello"): 
-    st.write("Hello, Mr. World")
+    st.write("Hello, Fuck you")
 else: 
     st.write("Good bye, ya")
